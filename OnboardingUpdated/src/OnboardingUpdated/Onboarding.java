003ff90d5bcb769cf1991a6fa92d97f17816b78b
aneliaslavtcheva <aneliaslavtcheva@usnyc36avtcheva.music.wmgis.com>
2017-11-28 16:07:29 -0500

    Almost done with the full on-boarding process

--- OnboardingUpdated/src/OnboardingUpdated/Onboarding.java
+++ OnboardingUpdated/src/OnboardingUpdated/Onboarding.java
@@ -1,6 +1,7 @@
 package OnboardingUpdated;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -31,19 +32,22 @@ public class Onboarding {
 		// Scroll inside web element vertically (e.g. 100 pixel)
 		//js.executeScript("arguments[0].scrollDown = arguments[1];",driver.findElement(By.className("hp-login-checkbox")), 500);
 		js.executeScript("arguments[0].scrollIntoView()",driver.findElement(By.className("hp-login-checkbox")));
+		
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
 		element.findElement(By.xpath("/html/body/app-root/app-startpage/div/app-startpage-login/div/app-startpage-register-form/div/form/div[7]/div/input")).click();
 		element.findElement(By.xpath("/html/body/app-root/app-startpage/div/app-startpage-login/div/app-startpage-register-form/div/form/div[6]/button")).click();
+		
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
 		//driver.navigate().back();
 		driver.findElement(By.xpath("//*[@id=\"md-input-0\"]")).sendKeys("Artist Name");
 		driver.findElement(By.xpath("//*[@id=\"md-input-1\"]")).sendKeys("First Artist Name");
@@ -51,52 +55,54 @@ public class Onboarding {
 		driver.findElement(By.xpath("//*[@id=\"md-input-3\"]")).sendKeys("01/01/2001");
 		driver.findElement(By.xpath("/html/body/app-root/app-on-boarding-root-page/main/md-horizontal-stepper/div[2]/div[1]/app-username-page/div/div[2]/button")).click();
 		
-		Select select = new Select(driver.findElement(By.xpath("/html/body/app-root/app-on-boarding-root-page/main/md-horizontal-stepper/div[2]/div[2]/app-contact-page/div/div[2]/div[1]/app-address-form/form/div[1]/div/md-select/div/span[1]")));
-		select.selectByVisibleText("American Samoa");
-		
-		//element.findElement(By.xpath("/html/body/app-root/app-startpage/div/app-startpage-login/div/app-startpage-login-form/div/form/div[4]")).click();
-		//try {
-		//	Thread.sleep(10000);
-		//} catch (InterruptedException e) {
-		//	e.printStackTrace();
-		//}
-		
-		//element.click();	
-		//driver.navigate().back();
+		driver.findElement(By.cssSelector(".mat-select-placeholder")).click();
+	    driver.findElement(By.id("md-option-3")).click();
+	    driver.findElement(By.xpath("//*[@id=\"md-input-4\"]")).sendKeys("1616 Broadway");
+	    driver.findElement(By.xpath("//*[@id=\"md-input-6\"]")).sendKeys("New York");
+	    driver.findElement(By.xpath("//*[@id=\"md-input-7\"]")).sendKeys("11224");
 		try {
+			Thread.sleep(8000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	    driver.findElement(By.xpath("/html/body/app-root/app-on-boarding-root-page/main/md-horizontal-stepper/div[2]/div[2]/app-contact-page/div/div[3]/button[2]")).click();
+	    try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	    driver.findElement(By.xpath("/html/body/app-root/app-on-boarding-root-page/main/md-horizontal-stepper/div[2]/div[3]/app-paypal-agreement-page/div/div[2]/app-privacy-policy-agreement-form/form/md-checkbox/label/div")).click();
+		driver.findElement(By.xpath("//*[@id=\"md-input-12\"]")).sendKeys("motheranelia@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"md-input-13\"]")).sendKeys("Nerses");
+		driver.findElement(By.xpath("//*[@id=\"md-input-14\"]")).sendKeys("Kanayan");
+		driver.findElement(By.xpath("//*[@id=\"md-input-15\"]")).sendKeys("nkanayan@gmail.com");
+		driver.findElement(By.cssSelector(".btn-link-paypal")).click();
+		try {
+			Thread.sleep(8000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// Initialize Javascript executor
+				JavascriptExecutor js1 = (JavascriptExecutor) driver;
+				// Scroll inside web element vertically (e.g. 100 pixel)
+				//js.executeScript("arguments[0].scrollDown = arguments[1];",driver.findElement(By.className("hp-login-checkbox")), 500);
+				js1.executeScript("arguments[0].scrollIntoView()",driver.findElement(By.className("mat-checkbox-inner-container")));
+		driver.findElement(By.cssSelector("#md-checkbox-3 > label:nth-child(1) > div:nth-child(1)")).click();
+		// Initialize Javascript executor
+		//JavascriptExecutor js2 = (JavascriptExecutor) driver;
+		// Scroll inside web element vertically (e.g. 100 pixel)
+		//js2.executeScript("arguments[0].scrollDown = arguments[1];",driver.findElement(By.className("controls-container step-section text-right")), 500);
+		//js2.executeScript("arguments[0].scrollIntoView()",driver.findElement(By.className("btn-next x5-ml mat-button mat-primary-medium")));
+		((WebElement) driver).sendKeys(Keys.DOWN);
+		((WebElement) driver).sendKeys(Keys.RETURN);
+		driver.findElement(By.xpath("/html/body/app-root/app-on-boarding-root-page/main/md-horizontal-stepper/div[2]/div[3]/app-paypal-agreement-page/div/div[6]/button[2]")).click();
+	try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		//driver.findElement(By.linkText("Sign Out")).click();
-		//driver.findElement(By.linkText("Sign in")).click();
-		//driver.findElement(By.xpath("//*[@id=\"md-input-0\"]")).sendKeys("dsp.testuser3@wmg.com");
-		//driver.findElement(By.xpath("//*[@id=\"md-input-1\"]")).sendKeys("Google124");
-		//driver.findElement(By.xpath("/html/body/app-root/app-startpage/div/app-startpage-login/div/app-startpage-login-form/div/form/div[4]")).click();
-		//try {
-		//	Thread.sleep(5000);
-		//} catch (InterruptedException e) {
-		//	e.printStackTrace();
-		//}
-		//driver.findElement(By.xpath("/html/body/app-root/app-main-nav/nav/div/div[2]/div[3]/span")).click();
-		//try {
-		//	Thread.sleep(5000);
-		//} catch (InterruptedException e) {
-		//	e.printStackTrace();
-		//}
-		//driver.findElement(By.cssSelector("#md-input-0")).clear();
-		//driver.findElement(By.cssSelector("#md-input-0")).sendKeys("100");
-		//try {
-		//	Thread.sleep(5000);
-		//} catch (InterruptedException e) {
-		//	e.printStackTrace();
-		//}
-		//driver.findElement(By.xpath("//*[@id=\"md-input-2\"]")).sendKeys("Google124");
-		//driver.findElement(By.xpath("/html/body/app-root/app-sales/div/div[1]/div[1]/app-withdraw-income/div/form/div[2]/button")).click();
 		
-		//element.findElement(By.xpath("/html/body/app-root/app-startpage/div/app-external-menu/nav/ul/li[2]/a")).click();
-		//WebDriverWait wait = new WebDriverWait(driver, 10); 
-		//WebElement messageElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("New Release")));
-	}
+		}
 
 }
